a0dbb7b5d3daf3ee8153f070f0414655d0241969
郑老师 <zjw_2301211@126.com>
2019-07-19 15:51:47 +0800

    master分支简单修改

--- Hello.java
+++ Hello.java
@@ -3,6 +3,16 @@ public class Hello{
 		System.out.println("Hello World!");
 	}
 	
+	public static void main(String[] args){	
+		System.out.println("Hello World!");
+	}
+	
+	// 测试方法
+	public void test(){
+		serr("测试测试!!!");
+		System.out.println("测试方法!");
+		System.out.println("master!!!!!");
+	}
 	// 测试方法
 	public void test(){
 		serr("测试测试!!!");
